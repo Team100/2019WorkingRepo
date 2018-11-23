a107293a8269783870175448435d35410ec1ca3a
Alex Beaver <31376007+alexanderbeaver799710@users.noreply.github.com>
2018-11-23 13:01:38 -0800

    Add JUnit Demo Code

---
+++ Alex B/JUnit_Sample/junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalculatorTests {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+
+	/// NOT PART OF ORIGINAL DEMO CODE
+	@Test
+	@DisplayName("subtraction")
+	void subtractTwoNumbers(){
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.subtract(6, 4), "Should be 2");
+		assertEquals(1000,calculator.subtract(4000,3000), "Should be 1000");
+	}
+
+	/// END
+	/* 
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+			"0,    1,   1",
+			"1,    2,   3",
+			"49,  51, 100",
+			"1,  100, 101"
+	})
+
+
+	void add(int first, int second, int expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+	*/
+}
