c2134841c2111057fcf164a4b00315f8d16d1868
asacco796940 <saccoa54@gmail.com>
2019-01-19 13:42:13 -0800

    Make Constants file and have working OI

---
+++ Breadboard/src/main/java/org/usfirst/frc100/Breadboard/Constants.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc100.Breadboard;
+
+/**
+ * Add your docs here.
+ */
+public class Constants {
+
+    public static int DRIVE_TRAIN_LEFT_MASTER_CANID = 20;
+    public static int DRIVE_TRAIN_LEFT_FOLLOWER_CANID = 1;
+    public static int DRIVE_TRAIN_RIGHT_MASTER_CANID = 15;
+    public static int DRIVE_TRAIN_RIGHT_FOLLOWER_CANID = 14;
+    public static int BALL_ROLLER_INTAKE_PWM = 8;
+
+}
--- Breadboard/src/main/java/org/usfirst/frc100/Breadboard/Robot.java
+++ Breadboard/src/main/java/org/usfirst/frc100/Breadboard/Robot.java
@@ -105,6 +105,7 @@ public class Robot extends TimedRobot {
     public void teleopInit() {
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
+        
         // continue until interrupted by another command, remove
         // this line or comment it out.
         if (autonomousCommand != null) autonomousCommand.cancel();
@@ -115,6 +116,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
+
         Scheduler.getInstance().run();
     }
 }
