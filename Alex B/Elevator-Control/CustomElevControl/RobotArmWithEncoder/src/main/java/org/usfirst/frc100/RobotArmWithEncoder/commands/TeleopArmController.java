388c17e8adab51d0a3a2a3b938b4dbaa582fa5d4
Alex Beaver <799710@seq.org>
2019-01-28 17:56:58 -0800

    State Machine Pseudocode

--- Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/commands/TeleopArmController.java
+++ Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/commands/TeleopArmController.java
@@ -7,6 +7,11 @@
 
 package org.usfirst.frc100.RobotArmWithEncoder.commands;
 
+import org.usfirst.frc100.RobotArmWithEncoder.Robot;
+import org.usfirst.frc100.RobotArmWithEncoder.subsystems.RobotArm;
+import org.usfirst.frc100.RobotArmWithEncoder.subsystems.RobotArm.HomeStates;
+import org.usfirst.frc100.RobotArmWithEncoder.subsystems.RobotArm.States;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class TeleopArmController extends Command {
@@ -23,6 +28,96 @@ public class TeleopArmController extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    switch(Robot.robotArm.state){
+      case INIT:
+        Robot.robotArm.homeState = HomeStates.NOT_STARTED;
+        Robot.robotArm.state = States.HOMING;
+        break;
+      case HOMING:
+        home();
+        break;
+      case GOING_DOWN:
+        break;
+      case DOWN:
+        break;
+      case GOING_UP:
+        break;
+      case UP:
+        break;
+      case TELEOP:
+        break;
+      case ERROR:
+        break;
+
+    }
+  }
+
+  private void home(){
+    switch(Robot.robotArm.homeState){
+      case NOT_STARTED:
+        //If Limit Switch Pressed... set to "At Limit Switch"
+        //Else Set to "Lowering to Limit Switch"
+
+        /*
+        if(limitSwitch.isPressed()){
+          Robot.robotArm.homeState=HomeStates.ELEV_AT_LIMIT_SWITCH;
+        }
+        else{
+          Robot.robotArm.homeState = HomeStates.ELEV_LOWERING_TO_LIMIT_SWITCH;
+          Robot.robotArm.robotArmMotor.set(-0.33);
+        }
+        */
+        
+        break;
+
+      case  ELEV_LOWERING_TO_LIMIT_SWITCH:
+        //If Limit Switch Pressed... set to "At Limit Switch"
+        //If timer expires... stop everything
+        /*
+        if(limitSwitch.isPressed()){
+          Robot.robotArm.homeState = HomeStates.ELEV_AT_LIMIT_SWITCH;
+          Robot.robotArm.set(0);
+        }
+        else if (timer.expired()){
+          Robot.robotArm.homeState = HomeStates.FATAL;
+          Robot.robotArm.set(0);
+        }
+        */
+        break;
+
+      case ELEV_AT_LIMIT_SWITCH:
+        //Transition to raising above limit switch
+        //Start timer
+        /*
+         * Robot.robotArm.set(0.33);
+         * Robot.robotArm.homeState = HomeStates.ELEV_RAISING_TO_ABOVE_LIMIT_SWITCH;
+         */
+        break;
+
+      case ELEV_RAISING_TO_ABOVE_LIMIT_SWITCH:
+        //When limit switch not pressed...transition to COMPLETE
+        //If timer expires... stop everything
+
+        /*
+         * if(!limitSwitch.isPressed()){
+         *    Robot.robotArm.set(0);
+         *    Robot.robotArm.homeState = HomeStates.COMPLETE;
+         * } 
+         */
+        
+        break;
+
+      case COMPLETE:
+        Robot.robotArm.state = States.DOWN;
+        break;
+
+      case FATAL:
+        Robot.robotArm.state = States.ERROR;
+        break;
+      default:
+        System.out.println("DEFAULT CASE FOR TELEOP ARM CONTROLLER REACHED... IS THIS INTENTIONAL?");
+
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
