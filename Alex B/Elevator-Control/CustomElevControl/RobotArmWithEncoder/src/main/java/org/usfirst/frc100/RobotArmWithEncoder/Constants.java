c8c103ddcbee35e549afb2b971b770832bcdc7ce
Alex Beaver <799710@seq.org>
2019-01-31 17:31:58 -0800

    Add basic elevator functionality (#48)

--- Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/Constants.java
+++ Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/Constants.java
@@ -5,25 +5,31 @@ public class Constants{
      * ARM ENCODER BUFFERS
      */
     public static int ARM_ENCODER_LOWER_BUFFER = 100;
-    public static int ARM_ENCODER_MAX_VALUE = 100;
-    public static int ARM_ENCODER_TOP_BUFFER = 100;
+    public static int ARM_ENCODER_MAX_VALUE = 55000;
+    public static int ARM_ENCODER_TOP_BUFFER = 1000;
 
     /*
      * ARM MOVEMENT DURATIONS
      */
-    public static double ROBOT_ELEVATOR_STATE_CONTROLLER_MAX_TRANSITION_TIME_TOP_BOTTOM = 3000;
+    public static double ROBOT_ELEVATOR_STATE_CONTROLLER_MAX_TRANSITION_TIME_TOP_BOTTOM = 7500;
     
     /*
      * HOMING Config
      */
 
-    public static double ARM_MOTOR_HOMING_POWER = 0.33;
+    public static double ARM_MOTOR_HOMING_POWER = 0.5;
     public static int HOMING_GOING_DOWN_MAX_DURATION = 750;
     public static int HOMING_GOING_UP_MAX_DURATION = 750;
 
     /*
      * ARM MOVEMENT SPEEDS
      */
-    public static double ELEVATOR_UP_SPEED = 0.5;
-    public static double ELEVATOR_DOWN_SPEED = -0.5;
+    public static double ELEVATOR_UP_SPEED = 1;
+    public static double ELEVATOR_DOWN_SPEED = 1;
+
+    /*
+     * ARM MOVEMENT MODIFIERS
+     */
+    public static int ELEVATOR_DOWN_MODIFIER = 1;
+    public static int ELEVATOR_UP_MODIFIER = -1;
 }
--- Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/commands/TeleopArmController.java
+++ Alex B/Elevator-Control/CustomElevControl/RobotArmWithEncoder/src/main/java/org/usfirst/frc100/RobotArmWithEncoder/commands/TeleopArmController.java
@@ -17,39 +17,73 @@ import org.usfirst.frc100.RobotArmWithEncoder.subsystems.RobotArm.States;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TeleopArmController extends Command {
   private Timer t;
   private Timer homerTimer;
   private enum JoystickButtons{
-    GO_UP,GO_DOWN,NONE
+    GO_UP,GO_DOWN,NONE,TELEOP
   }
   private JoystickButtons joy =  JoystickButtons.NONE;
   private int cyclesInErrorState;
   public TeleopArmController() {
     // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+    requires(Robot.robotArm);
+    t = new Timer();
+    homerTimer = new Timer();
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    System.out.println("TeleopArmController INIT");
     t.reset();
     t.start();
+    Robot.robotArm.state = States.INIT;
+    Robot.robotArm.homeState = HomeStates.NOT_STARTED;
   }
 
   // Called repeatedly when this Command is scheduled to run
+
+  private void generalArmButtonHandler(){
+    if(joy == JoystickButtons.GO_UP){
+      Robot.robotArm.state = States.GOING_UP;
+      Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_UP_MODIFIER*Constants.ELEVATOR_UP_SPEED);
+    }
+    else if(joy == JoystickButtons.GO_DOWN){
+      System.out.println("HEADING DOWN");
+      Robot.robotArm.state = States.GOING_DOWN;
+      Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_DOWN_MODIFIER*Constants.ELEVATOR_DOWN_SPEED);
+    }
+    else if(joy == JoystickButtons.TELEOP){
+      Robot.robotArm.state = States.TELEOP;
+      
+    }
+
+  }
   @Override
   protected void execute() {
+    SmartDashboard.putString("Robot Arm State", Robot.robotArm.state.toString());
+    SmartDashboard.putBoolean("Lower Switch Triggered", Robot.robotArm.lowerLimitSwitch.get());
+    SmartDashboard.putBoolean("Upper Switch Triggered", Robot.robotArm.upperLimitSwitch.get());
+    SmartDashboard.putNumber("ENC VALUE", Robot.robotArm.robotArmEncoder.get());
     joy = JoystickButtons.NONE;
     if(Robot.oi.up.get()){
+      System.out.println("GO UP Pressed");
       joy = JoystickButtons.GO_UP;
     }
     else if (Robot.oi.down.get()){
       joy = JoystickButtons.GO_DOWN;
+      System.out.println("GO DOWN Pressed");
+    }
+    else if (Robot.oi.teleop.get()){
+      joy = JoystickButtons.TELEOP;
+      System.out.println("TELEOP Pressed");
     }
     switch(Robot.robotArm.state){
       case INIT:
+        System.out.println("INIT");
         Robot.robotArm.homeState = HomeStates.NOT_STARTED;
         Robot.robotArm.state = States.HOMING;
         break;
@@ -57,6 +91,7 @@ public class TeleopArmController extends Command {
         home();
         break;
       case GOING_DOWN:
+        generalArmButtonHandler();
         if(Robot.robotArm.robotArmEncoder.get() < Constants.ARM_ENCODER_LOWER_BUFFER){
           Robot.robotArm.robotArmMotor.set(0);
           Robot.robotArm.state = States.DOWN;
@@ -69,12 +104,10 @@ public class TeleopArmController extends Command {
         }
         break;
       case DOWN:
-        if(joy == JoystickButtons.GO_UP){
-          Robot.robotArm.state = States.GOING_UP;
-          Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_UP_SPEED);
-        }
+        generalArmButtonHandler();
         break;
       case GOING_UP:
+        generalArmButtonHandler();
         if(t.get() > Constants.ROBOT_ELEVATOR_STATE_CONTROLLER_MAX_TRANSITION_TIME_TOP_BOTTOM){
           Robot.robotArm.state = States.ERROR;
           System.out.println("ERROR RECIEVED AT FPGA:"+Timer.getFPGATimestamp()+" GOING_UP_STATE_MACHINE_TIMER_EXCEEDED");
@@ -83,22 +116,36 @@ public class TeleopArmController extends Command {
         }
         if(Robot.robotArm.robotArmEncoder.get() > Constants.ARM_ENCODER_MAX_VALUE-Constants.ARM_ENCODER_TOP_BUFFER){
           Robot.robotArm.state = States.UP;
+          Robot.robotArm.robotArmMotor.set(0);
+          t.reset();
+        }
+        if(Robot.robotArm.upperLimitSwitch.get()){
+          Robot.robotArm.state = States.UP;
+          Robot.robotArm.robotArmMotor.set(0);
           t.reset();
         }
         break;
       case UP:
-        if(joy == JoystickButtons.GO_DOWN){
-          Robot.robotArm.state = States.GOING_DOWN;
-          Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_DOWN_SPEED);
-        }
+        generalArmButtonHandler();
         break;
       case TELEOP:
-        if(Robot.robotArm.limitSwitch.get()){
+        generalArmButtonHandler();
+        if(Robot.robotArm.lowerLimitSwitch.get() && Robot.oi.teleopJoystick.getY() > 0){
+          Robot.robotArm.robotArmMotor.set(0);
+        }
+        else if(Robot.robotArm.upperLimitSwitch.get() && Robot.oi.teleopJoystick.getY() < 0){
+          Robot.robotArm.robotArmMotor.set(0);
+        }
+        else if(Robot.robotArm.robotArmEncoder.get() < Constants.ARM_ENCODER_LOWER_BUFFER && Robot.oi.teleopJoystick.getY() > 0){
+          Robot.robotArm.robotArmMotor.set(0);
+        }
+        else if(Robot.robotArm.robotArmEncoder.get()>Constants.ARM_ENCODER_MAX_VALUE-Constants.ARM_ENCODER_TOP_BUFFER && Robot.oi.teleopJoystick.getY() < 0){
           Robot.robotArm.robotArmMotor.set(0);
         }
         else{
-          Robot.robotArm.robotArmMotor.set(Robot.oi.teleopJoystick.getX());
+          Robot.robotArm.robotArmMotor.set(Robot.oi.teleopJoystick.getY());
         }
+
         
         break;
       case ERROR:
@@ -114,15 +161,17 @@ public class TeleopArmController extends Command {
       case NOT_STARTED:
         //If Limit Switch Pressed... set to "At Limit Switch"
         //Else Set to "Lowering to Limit Switch"
-
+        System.out.println("Starting Home");
         homerTimer.reset();
         homerTimer.start();
-        if(Robot.robotArm.limitSwitch.get()){
+        if(Robot.robotArm.lowerLimitSwitch.get()){
+          System.out.println("Limit Switch triggered");
           Robot.robotArm.homeState=HomeStates.ELEV_AT_LIMIT_SWITCH;
         }
         else{
+          System.out.println("Limit Switch not triggered");
           Robot.robotArm.homeState = HomeStates.ELEV_LOWERING_TO_LIMIT_SWITCH;
-          Robot.robotArm.robotArmMotor.set(-1*Constants.ARM_MOTOR_HOMING_POWER);
+          Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_DOWN_MODIFIER*Constants.ARM_MOTOR_HOMING_POWER);
         }
         
         
@@ -132,11 +181,13 @@ public class TeleopArmController extends Command {
         //If Limit Switch Pressed... set to "At Limit Switch"
         //If timer expires... stop everything
         
-        if(Robot.robotArm.limitSwitch.get()){
+        if(Robot.robotArm.lowerLimitSwitch.get()){
+          System.out.println("Limit Switch triggered");
           Robot.robotArm.homeState = HomeStates.ELEV_AT_LIMIT_SWITCH;
           Robot.robotArm.robotArmMotor.set(0);
         }
         else if (homerTimer.get() >= Constants.HOMING_GOING_DOWN_MAX_DURATION){
+
           Robot.robotArm.homeState = HomeStates.FATAL;
           System.out.println("ERROR RECIEVED AT FPGA:"+Timer.getFPGATimestamp()+" HOMING_GOING_DOWN_STATE_MACHINE_TIMER_EXCEEDED");
 
@@ -152,7 +203,7 @@ public class TeleopArmController extends Command {
         homerTimer.stop();
         homerTimer.reset();
         homerTimer.start();
-        Robot.robotArm.robotArmMotor.set(Constants.ARM_MOTOR_HOMING_POWER);
+        Robot.robotArm.robotArmMotor.set(Constants.ELEVATOR_UP_MODIFIER*Constants.ARM_MOTOR_HOMING_POWER);
         Robot.robotArm.homeState = HomeStates.ELEV_RAISING_TO_ABOVE_LIMIT_SWITCH;
          
         break;
@@ -161,7 +212,7 @@ public class TeleopArmController extends Command {
         //When limit switch not pressed...transition to COMPLETE
         //If timer expires... stop everything
 
-        if(!Robot.robotArm.limitSwitch.get()){
+        if(!Robot.robotArm.lowerLimitSwitch.get()){
           Robot.robotArm.robotArmMotor.set(0);
           Robot.robotArm.homeState = HomeStates.COMPLETE;
         } 
@@ -176,6 +227,8 @@ public class TeleopArmController extends Command {
         Robot.robotArm.state = States.DOWN;
         Robot.robotArm.homeState = HomeStates.NOT_STARTED;
         homerTimer.stop();    
+        Robot.robotArm.robotArmEncoder.reset();
+        System.out.println("HOME Complete");
         break;
 
       case FATAL:
