7571e9fc416cbc731c5d3793b802257177365877
GDW1 <31372594+GDW1@users.noreply.github.com>
2019-02-02 12:34:25 -0800

    typo

--- Breadboard/src/main/java/org/usfirst/frc100/Breadboard/subsystems/Drivetrain.java
+++ Breadboard/src/main/java/org/usfirst/frc100/Breadboard/subsystems/Drivetrain.java
@@ -85,7 +85,7 @@ public class Drivetrain extends Subsystem implements PIDOutput{
         turnPID.setContinuous(true);
         turnPID.setOutputRange(Constants.DT_TURN_MIN_OUTPUT, Constants.DT_TURN_MAX_OUTPUT);
         turnPID.setAbsoluteTolerance(Constants.DT_TURN_ABSOLUTE_TOLERANCE);
-        leftMaster.setSensorPahse(true);
+        leftMaster.setSensorPhase(true);
         rightMaster.setSensorPhase(true);
     }
     
