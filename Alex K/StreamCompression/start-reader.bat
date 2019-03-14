7c205c1c6ee0e12bd040aa4307c3e3fcaae20a9c
Alex Krantz <alex@alexkrantz.com>
2019-03-13 18:38:35 -0700

    Enlarge resolution

--- Alex K/StreamCompression/start-reader.bat
+++ Alex K/StreamCompression/start-reader.bat
@@ -1,1 +1,1 @@
-start "Vision" "C:\Program Files\Python37\python.exe" reader.py --host raspberrypi.local --port 5810 --upscale-width 640 --upscale-height 480 --rotate 180
+start "Vision" "C:\Program Files\Python37\python.exe" reader.py --host raspberrypi.local --port 5805 --upscale-width 1280 --upscale-height 960 --rotate 180
