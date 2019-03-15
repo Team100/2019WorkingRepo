337775664c5b2cc9d6eb08719477a97fe75530ef
Alex Krantz <alex@alexkrantz.com>
2019-03-15 16:27:03 -0700

    Add reversing camera product ids

--- Alex K/StreamCompression/startup.py
+++ Alex K/StreamCompression/startup.py
@@ -6,18 +6,19 @@ parser = argparse.ArgumentParser(description="Start all of the vision processes 
 parser.add_argument("--link-local", help="Change to using IPv6 for link-local support", action="store_true")
 parser.add_argument("--disable-vision", help="Stop vision process from running", action="store_true")
 parser.add_argument("--disable-driver", help="Stop driver process from running", action="store_true")
+parser.add_argument("--reverse", help="Reverse camera product IDs", action="store_true")
 args = parser.parse_args()
 
 device_info = {dev: {property.split("=")[0][3:].lower(): property.split("=")[1] for property in subprocess.run("udevadm info -n {device} -q property".format(device=dev), stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8").strip().split("\n") if "ID_MODEL_ID" in property or "ID_VENDOR_ID" in property} for dev in subprocess.run("ls /dev/video*", stdout=subprocess.PIPE, shell=True).stdout.decode("utf-8").strip().split("\n")}
 
 camera_assignments = {"vision": "", "driver": []}
 for camera in device_info:
-	if device_info[camera]["model_id"] == "0810":
+	if device_info[camera]["model_id"] == ("0810" if args.reverse else "0779"):
 		if len(camera_assignments["driver"]) != 2:
 			camera_assignments["driver"].append(camera)
 		else:
 			raise ValueError("too many driver cameras connected")
-	elif device_info[camera]["model_id"] == "0779":
+	elif device_info[camera]["model_id"] == ("0779" if args.reverse else "0810"):
 		if camera_assignments["vision"] == "":
 			camera_assignments["vision"] = camera
 		else:
